6d6285355c87aaa9ee9cf76c656ad4731bd7d9d3
allan <allan.lelous@gmail.com>
2021-11-11 14:48:12 +0100

    Ajout du style de la page Home

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 class HomeScreen extends Component {
     render() {
         return (
-            <View>
-                <Text>Rechercher par : </Text>
-                <TouchableOpacity
-                onPress = {() => this.props.navigation.navigate("Category")}
-                >
-                    <Text>Catégorie</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                onPress = {() => this.props.navigation.navigate("Software")}
-                >
-                    <Text>Logiciel</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
+            <View style={styles.container}>
+                <Text style={styles.mainText}>Rechercher par : </Text>
+
+                <View style={styles.containerButton}>
+                    {/* Ajout du Bouton Catégorie */}
+                    <TouchableOpacity style={styles.buttons}
+                    onPress = {() => this.props.navigation.navigate("Category")}
+                    >
+                        <Text style={styles.buttonsText}>Catégorie</Text>
+                    </TouchableOpacity>
+                    {/* Ajout du Bouton Logiciel */}
+                    <TouchableOpacity style={styles.buttons}
+                    onPress = {() => this.props.navigation.navigate("Software")}
+                    >
+                        <Text style={styles.buttonsText}>Logiciel</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.mainText}>Ou bien : </Text>
+                {/* Ajout du Bouton Ajouter un raccourci */}
+                <TouchableOpacity style={[styles.buttons, styles.shortcutButton]}
                 onPress = {() => this.props.navigation.navigate("Shortcuts")}
                 >
-                    <Text>Ajouter un raccourci</Text>
+                    <Text style={[styles.buttonsText, styles.shortcutText]}>Ajouter un raccourci</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -27,3 +35,54 @@ class HomeScreen extends Component {
 }
 
 export default HomeScreen;
+
+// Style de la page HOME
+const styles = StyleSheet.create ({
+    container: {
+    display: 'flex',
+    flex:1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#DBE0DE',
+    },
+    mainText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#114A8A',
+    },
+    buttonsText: {
+        fontSize:18,
+        fontWeight: '500',
+        color: '#114A8A',
+    },
+    containerButton: {
+        display:'flex',
+        flexDirection: 'row',
+    },
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor : 'transparent',
+        borderStyle: 'solid',
+        borderColor: '#114A8A',
+        borderWidth: 2,
+        width: 160,
+        height: 75,
+        borderRadius: 50,
+        marginHorizontal: 15,
+    },
+    shortcutButton: {
+        backgroundColor : '#114A8A',
+        width:350,
+        height:100,
+        borderRadius: 50,
+        marginBottom: 50,
+    },
+    shortcutText: {
+        color : 'white',
+        fontSize: 20,
+    },
+})
+
